3b2d9e81cb955bf4425cec959862fcad59fdbefd
gouz7514 <gouz7514@gmail.com>
2023-11-13 22:35:55 +0900

    Test : util 테스트 코드 추가

---
+++ __tests__/util/orderTest.js
@@ -0,0 +1,17 @@
+import { orderToArray } from "../../src/util/order.js";
+
+describe("util/order 테스트", () => {
+  describe("orderToArray 메소드는 주어진 주문을 배열로 변환한다.", () => {
+    test("티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1", () => {
+      const orders = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+      const expected = [
+        { menu: "티본스테이크", amount: "1" },
+        { menu: "바비큐립", amount: "1" },
+        { menu: "초코케이크", amount: "2" },
+        { menu: "제로콜라", amount: "1" },
+      ];
+      const result = orderToArray(orders);
+      expect(result).toEqual(expected);
+    });
+  });
+})
