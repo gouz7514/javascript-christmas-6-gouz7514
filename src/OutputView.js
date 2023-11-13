384efa575f86d639dcd74acf9cb1cd3813f58e01
gouz7514 <gouz7514@gmail.com>
2023-11-13 19:38:27 +0900

    Refactor : outputHelper를 사용해 출력 메시지 결정

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,6 +1,6 @@
 import { Console } from "@woowacourse/mission-utils";
-import { OUTPUT, UNIT, EMPTY_CASE } from "./constants/message.js";
-import { BENEFIT } from "./constants/constant.js";
+import { OUTPUT } from "./constants/message.js";
+import outputHelper from "./util/outputHelper.js";
 
 const OutputView = {
   startOrder() {
@@ -12,7 +12,7 @@ const OutputView = {
   // 3. 최종 정보를 출력한다.
   printBill(bill) {
     const { visitDate, orders, totalPrice, giveAway, benefits, benefitAmount, badge, finalPrice } = bill.calculateBill();
-    Console.print(OUTPUT.printBill(visitDate));
+    Console.print(outputHelper.printBill(visitDate));
     this.printOrders(orders);
     this.printTotalPrice(totalPrice);
     this.printGiveAway(giveAway);
@@ -23,61 +23,31 @@ const OutputView = {
   },
   // 3-1. 주문 메뉴를 출력한다.
   printOrders(orders) {
-    Console.print(OUTPUT.printOrders);
-    orders.forEach((order) => {
-      const { menu, amount } = order;
-      Console.print(`${menu} ${amount}${UNIT.amount}`);
-    });
+    Console.print(outputHelper.printOrders(orders));
   },
   // 3-2. 할인 전 총주문 금액을 출력한다.
   printTotalPrice(totalPrice) {
-    Console.print(OUTPUT.printTotalPrice);
-    Console.print(`${totalPrice.toLocaleString()}${UNIT.price}`);
+    Console.print(outputHelper.printTotalPrice(totalPrice));
   },
   // 3-3. 증정 메뉴를 출력한다.
   printGiveAway(giveAway) {
-    Console.print(OUTPUT.printGiveAway);
-    if (!giveAway) {
-      Console.print(EMPTY_CASE.giveAway);
-      return;
-    }
-    Console.print(`${BENEFIT.giveAway.menu} ${BENEFIT.giveAway.amount}${UNIT.amount}`);
+    Console.print(outputHelper.printGiveAway(giveAway));
   },
   // 3-4. 혜택 내역을 출력한다.
   printBenefits(benefits) {
-    Console.print(OUTPUT.printBenefits);
-    if (!benefits || !benefits.length) {
-      Console.print(EMPTY_CASE.benefits);
-      return;
-    }
-    benefits.forEach((benefit) => {
-      const { name, discount } = benefit;
-      Console.print(`${BENEFIT[name].name_ko}: -${discount.toLocaleString()}${UNIT.price}`);
-    });
+    Console.print(outputHelper.printBenefits(benefits));
   },
   // 3-5. 총혜택 금액을 출력한다.
   printBenefitAmount(benefitAmount) {
-    Console.print(OUTPUT.printBenefitAmount);
-    if (!benefitAmount) {
-      Console.print(EMPTY_CASE.benefitAmount);
-      return;
-    }
-    Console.print(`-${benefitAmount.toLocaleString()}${UNIT.price}`);
+    Console.print(outputHelper.printBenefitAmount(benefitAmount));
   },
   // 3-6. 할인 후 예상 결제 금액을 출력한다.
   printFinalPrice(finalPrice) {
-    Console.print(OUTPUT.printFinalPrice);
-    Console.print(`${finalPrice.toLocaleString()}${UNIT.price}`);
+    Console.print(outputHelper.printFinalPrice(finalPrice));
   },
   // 3-7. 12월 이벤트 배지를 출력한다.
   printBadge(badge) {
-    Console.print(OUTPUT.printBadge);
-    if (!badge) {
-      Console.print(EMPTY_CASE.badge);
-      return;
-    }
-    const { info: { name: badgeName } } = badge;
-    Console.print(`${badgeName}`);
+    Console.print(outputHelper.printBadge(badge));
   },
 };
 
---
+++ src/util/outputHelper.js
@@ -0,0 +1,62 @@
+import { OUTPUT, UNIT, EMPTY_CASE } from "../constants/message.js";
+import { BENEFIT } from "../constants/constant.js";
+
+const outputHelper = {
+  printBill(date) {
+    return OUTPUT.printBill(date);
+  },
+  printOrders(orders) {
+    return orders.reduce((acc, cur) => {
+      const { menu, amount } = cur;
+      return `${acc}\n${menu} ${amount}${UNIT.amount}`;
+    }, OUTPUT.printOrders);
+  },
+  printTotalPrice(price) {
+    return `${OUTPUT.printTotalPrice}\n${price.toLocaleString()}${UNIT.price}`;
+  },
+  printGiveAway(giveAway) {
+    let result = OUTPUT.printGiveAway;
+    if (!giveAway) {
+      result += `\n${EMPTY_CASE.giveAway}`;
+      return result;
+    }
+    result += `\n${BENEFIT.giveAway.menu} ${BENEFIT.giveAway.amount}${UNIT.amount}`;
+    return result;
+  },
+  printBenefits(benefits) {
+    let result = OUTPUT.printBenefits;
+    if (!benefits || !benefits.length) {
+      result += `\n${EMPTY_CASE.benefits}`;
+      return result;
+    }
+    benefits.forEach((benefit) => {
+      const { name, discount } = benefit;
+      result += `\n${BENEFIT[name].name_ko}: -${discount.toLocaleString()}${UNIT.price}`;
+    });
+    return result;
+  },
+  printBenefitAmount(benefitAmount) {
+    let result = OUTPUT.printBenefitAmount;
+    if (!benefitAmount) {
+      result += `\n${EMPTY_CASE.benefitAmount}`;
+      return result;
+    }
+    result += `\n-${benefitAmount.toLocaleString()}${UNIT.price}`;
+    return result;
+  },
+  printFinalPrice(finalPrice) {
+    return `${OUTPUT.printFinalPrice}\n${finalPrice.toLocaleString()}${UNIT.price}`;
+  },
+  printBadge(badge) {
+    let result = OUTPUT.printBadge;
+    if (!badge) {
+      result += `\n${EMPTY_CASE.badge}`;
+      return result;
+    }
+    const { info: { name: badgeName } } = badge;
+    result += `\n${badgeName}`;
+    return result;
+  }
+};
+
+export default outputHelper;
