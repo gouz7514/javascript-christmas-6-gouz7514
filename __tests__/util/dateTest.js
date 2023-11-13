df076ee4bbaf910497e42ab9b094d8216ad097e1
gouz7514 <gouz7514@gmail.com>
2023-11-14 02:22:42 +0900

    Test(fix) : 변경된 주말, 평일 기준을 dateTest에 반영

--- __tests__/util/dateTest.js
+++ __tests__/util/dateTest.js
@@ -1,33 +1,33 @@
-import { EVENT } from "../../src/constants/constant.js";
+import { DAY, EVENT } from "../../src/constants/constant.js";
 import { getDay, isWeekend, isWeekday, isSpecial } from "../../src/util/date.js";
 
 describe("util/date 테스트", () => {
   describe("getDay 메소드는 주어진 날짜에 해당하는 요일을 반환한다.", () => {
     test(`${EVENT.year}년 ${EVENT.month}월 1일은 금요일이다`, () => {
       const date = 1;
-      const expected = 5;
+      const expected = DAY.fri;
       const result = getDay(date);
       expect(result).toBe(expected);
     });
   });
 
   describe("isWeekend 메소드는 주어진 날짜가 주말인지 확인한다.", () => {
-    test("1일은 주말이 아니다.", () => {
+    test("1일은 주말이다.", () => {
       const date = 1;
       const result = isWeekend(date);
-      expect(result).toBe(false);
+      expect(result).toBe(true);
     });
 
-    test("2일은 주말이다.", () => {
-      const date = 2;
+    test("17일은 주말이다.", () => {
+      const date = 17;
       const result = isWeekend(date);
-      expect(result).toBe(true);
+      expect(result).toBe(false);
     });
   });
 
   describe("isWeekday 메소드는 주어진 날짜가 평일인지 확인한다.", () => {
-    test("1일은 평일이다.", () => {
-      const date = 1;
+    test("3일은 평일이다.", () => {
+      const date = 3;
       const result = isWeekday(date);
       expect(result).toBe(true);
     });
